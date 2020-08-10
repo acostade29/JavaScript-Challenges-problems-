8cf38d078f9b91bb9f3d5df1093acff28f5f5d6f
Alfonso D Rojas Hernandez <acostade@gmail.com>
2020-08-10 18:53:16 +0000

    reporting for duty problem

--- index.js
+++ index.js
@@ -94,3 +94,10 @@ const finalGrade = (midterm, final, homework) => {
         return 'A'
     }
 }
+
+// 6-Write a function, reportingForDuty(), that has two string parameters, rank and lastName, and returns a string in the following format: ‘rank lastName reporting for duty!’
+
+const reportingForDuty = (rank, lastName) => {
+return (`${rank} ${lastName} reporting for duty!`)
+}
+
