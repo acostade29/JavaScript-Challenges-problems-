b54bf748229c1d622282c3ed2709e0699ce2889b
Alfonso D Rojas Hernandez <acostade@gmail.com>
2020-08-10 22:04:39 +0000

    done problem 8

--- index.js
+++ index.js
@@ -143,3 +143,27 @@ const calculateWeight = (earthWeight, planet) => {
             return 'Invalid Planet Entry. Try: Mercury, Venus, Mars, Jupiter, or Saturn.'
     }
 }
+
+
+// 8-It can be hard to keep track of what’s truthy or falsy in JavaScript. Write a function, truthyOrFalsy(), that takes in any value and returns true if that value is truthy and false if that value is falsy.
+
+const truthyOrFalsy = value => {
+    if (value) {
+        return true
+    } 
+    return false
+}
+
+/*
+// As a function declaration:
+function truthyOrFalsy(value) {
+    if (value) {
+        return true
+    } else {
+        return false
+    }
+}
+
+// Using a ternary: 
+const truthyOrFalsy = value => value ? true : false 
+*/
